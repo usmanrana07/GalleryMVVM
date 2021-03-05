219177fb68cf2a520eb6803383826f2286e6a33d
usmanrana07 <usman.rana@ilsainteractive.com>
2021-03-05 14:36:02 +0500

    CommonUtils: removed unused methods

--- app/src/main/java/com/usman/gallerydemo/utils/CommonUtils.kt
+++ app/src/main/java/com/usman/gallerydemo/utils/CommonUtils.kt
@@ -1,35 +1,8 @@
 package com.usman.gallerydemo.utils
 
-import android.content.ContentResolver
-import android.content.Context
 import android.util.SparseArray
-import android.webkit.MimeTypeMap
-import androidx.core.net.toUri
-import java.io.File
-import java.util.*
 
 object CommonUtils {
-
-    fun getFileExtension(path: String): String {
-        return path.substring(path.lastIndexOf('.'))
-    }
-
-    fun getMimeType(context: Context, file: File): String {
-        val mimeType: String = if (file.toUri().scheme.equals(ContentResolver.SCHEME_CONTENT)) {
-            val cr = context.contentResolver
-            cr.getType(file.toUri()).toString()
-        } else {
-            val extension = MimeTypeMap.getFileExtensionFromUrl(file.toUri().toString())
-            MimeTypeMap.getSingleton().getMimeTypeFromExtension(
-                extension.toLowerCase(
-                    Locale.ROOT
-                )
-            ).toString()
-        }
-        AppLogger.d("file_mimeType", "type = $mimeType")
-
-        return mimeType
-    }
 
     fun <C> asList(sparseArray: SparseArray<C>): MutableList<C> {
         val arrayList = arrayListOf<C>()
